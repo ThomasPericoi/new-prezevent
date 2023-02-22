5c4f30604644380a785034cf3acba377ac1b28d5
Thomas Pericoi <thomaspericoi@gmail.com>
2023-02-22 20:52:16 +0100

    Bmlock | Add Shortcode

---
+++ admin/blocks/shortcode/shortcode.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Shortcode Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during backend preview render.
+ * @param   int $post_id The post ID the block is rendering content against.
+ *          This is either the post ID currently being displayed inside a query loop,
+ *          or the post ID of the post hosting this block.
+ * @param   array $context The context provided to the block by the post or it's parent block.
+ */
+
+$shortcode = get_field("shortcode");
+
+$classes = 'shortcode-block';
+if (!empty($block['className'])) {
+    $classes .= ' ' . $block['className'];
+}
+
+$styles = array("");
+$style  = implode('; ', $styles);
+
+?>
+<!-- Block - Shortcode -->
+<section class="<?php echo esc_attr($classes); ?>">
+    <div class="container">
+        <?php echo $shortcode; ?>
+    </div>
+</section>
